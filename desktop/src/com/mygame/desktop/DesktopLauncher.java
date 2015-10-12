9ef38531b430646951d51289dfacf10af9863e0b
Chaow Porkaew <pchaowmobile@gmail.com>
2015-10-12 08:36:17 +0700

    initial project

---
+++ desktop/src/com/mygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygame.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
